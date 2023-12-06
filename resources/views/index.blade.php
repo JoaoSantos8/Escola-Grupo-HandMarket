9874e9eeb05fa0193b2bba095e2f0f5046ef8d69
JoaoSantos8 <100931229+JoaoSantos8@users.noreply.github.com>
2023-12-06 12:52:45 +0000

    Index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,13 +3,65 @@
 @section('title','Home')
 
 @section('styles')
+
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
 @endsection
 
 @section('content')
+<style>
 
-<h1>HomePage</h1>
+
+    box-sizing: none !important;
+
+</style>
+<div class="rotate-slider">
+    <ul class="slides">
+      <li>
+        <div class="inner">
+          <h2>Actual Rotating Slider</h2>
+          <p>Built with math and dedication</p>
+        </div>
+      </li>
+      <li>
+        <div class="inner"></div>
+      </li>
+      <li>
+        <div class="inner">
+          <h2>Hates IE</h2>
+          <p>Uses clip-path to shape slides</p>
+        </div>
+      </li>
+      <li>
+        <div class="inner">
+        </div>
+      </li>
+      <li>
+        <div class="inner">
+          <h2>Easy to use</h2>
+          <p>Just add list elements in the HTML and set a height and width in the JavaScript Settings</p>
+          <p><small>Slider controls and proper jQuery plugin coming soon.</small></p>
+        </div>
+      </li>
+      <li>
+        <div class="inner">
+          <h2>I'm not a thief!</h2>
+          <p>Stock photos from www.pexels.com</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <svg>
+    <defs>
+      <clipPath id="slideClip">
+        <path />
+      </clipPath>
+    </defs>
+  </svg>
 
 @endsection
 
 @section('scripts')
+
+<script src="{{ asset('js/index.js') }}"></script>
+
 @endsection
