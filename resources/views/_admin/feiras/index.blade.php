d341435eb4c8a37d3c55945efbe5ae90ac6d9ad2
JoaoSantos8 <100931229+JoaoSantos8@users.noreply.github.com>
2023-12-15 09:57:39 +0000

    Revisao2

--- resources/views/_admin/feiras/index.blade.php
+++ resources/views/_admin/feiras/index.blade.php
@@ -3,6 +3,7 @@
 
 @section("content")
 
+
 <div class="container-fluid">
   <h1 class="h3 mb-2 text-gray-800">Feiras</h1>
 
@@ -15,20 +16,32 @@
     <div class="card-body">
       @if (count($feiras))
       <div class="table-responsive">
-        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+        <table class="table table-bordered text-center" id="dataTable" width="100%" cellspacing="0">
           <thead>
             <tr>
-            <th>Imagem</th>
+            <th style="width: 20%">Imagem</th>
               <th>Nome</th>
-              <th></th>
+              <th>Descição</th>
+              <th>Data - Inicio</th>
+              <th>Data - Fim</th>
+              <th>Preço</th>
+              <th>Opções</th>
             </tr>
           </thead>
           <tbody>
             @foreach ($feiras as $feira)
             <tr>
-                <td><img class="w-50" src="{{asset('storage/imagens_feiras/'.$feira->feiraImagemURL)}}" alt="Feira img"></td>
-              <td>{{ $feira->feiraNome }}</td>
-              <td nowrap>
+                <td><img class="w-100" src="{{asset('storage/imagens_feiras/'.$feira->feiraImagemURL)}}" alt="Feira img"></td>
+              <td class="align-middle text-center">{{ $feira->feiraNome }}</td>
+              <td class="align-middle text-center p-5">{{ $feira->feiraDescricao }}</td>
+              <td class="align-middle text-center p-5">{{ $feira->feiraDataInicio }}</td>
+              <td class="align-middle text-center p-5">{{ $feira->feiraDataFim }}</td>
+              <td class="align-middle text-center p-5">@if ($feira->feiraPreco == 0)
+                Grátis
+            @else
+                {{ $feira->feiraPreco }}€
+            @endif</td>
+              <td nowrap class="align-middle text-center">
                 <a class="btn btn-xs btn-primary btn-p" href="{{ route('admin.feiras.show', $feira) }}"><i class="fas fa-eye fa-xs"></i></a>
                 <a class="btn btn-xs btn-warning btn-p" href="{{ route('admin.feiras.edit', $feira) }}"><i class="fas fa-pen fa-xs"></i></a>
                 <form method="POST" action="{{ route('admin.feiras.destroy', $feira) }}" role="form" class="inline" onsubmit="return confirm('Confirma que pretende eliminar este registo?');">
