17dea424d2ba35669395ea3e326d3a663dd9596b
JoaoSantos8 <100931229+JoaoSantos8@users.noreply.github.com>
2023-12-15 02:12:08 +0000

    Etapa 9 Concluida

--- resources/views/layout/partials/navbar.blade.php
+++ resources/views/layout/partials/navbar.blade.php
@@ -1,33 +1,37 @@
 <nav class="navbar">
-    <a href="{{route('index')}}"><img class="nav-logo" src="{{asset('fotos/logo.png')}}"
-            alt="Logo"></a>
+    <a href="{{ route('index') }}"><img class="nav-logo" src="{{ asset('fotos/logo.png') }}" alt="Logo"></a>
     <ul class="nav-menu">
         <li class="nav-item">
-            <a href="{{route('index')}}" class="nav-link">Home</a>
+            <a href="{{ route('index') }}" class="nav-link">Home</a>
         </li>
         <li class="nav-item">
             <a href="{{ route('feira') }}" class="nav-link">Feiras</a>
         </li>
-
         <li class="nav-item">
             <a href="#" class="nav-link">Artesãos</a>
         </li>
         <li class="nav-item">
-            <a href="{{route('patrocinadores')}}" class="nav-link">Patrocinadores</a>
+            <a href="{{ route('patrocinadores') }}" class="nav-link">Patrocinadores</a>
         </li>
         <li class="nav-item">
-            <a href="{{route('noticia')}}" class="nav-link">Noticias</a>
+            <a href="{{ route('noticia') }}" class="nav-link">Noticias</a>
         </li>
         <li class="nav-item">
             <a href="#" class="nav-link">Loja</a>
         </li>
-        <li class="nav-item">
-            <a href="{{ route('signup') }}" class="nav-link">signup</a>
-        </li>
-
-        <li class="nav-item">
-            <a href="{{ route('login') }}" class="nav-link">Login</a>
-        </li>
+        @auth
+            <li class="nav-item">
+                <a class="nav-link">{{ auth()->user()->name }}</a>
+            </li>
+        @endauth
+        @guest
+            <li class="nav-item">
+                <a href="{{ route('signup') }}" class="nav-link">Signup</a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ route('login') }}" class="nav-link">Login</a>
+            </li>
+        @endguest
     </ul>
     <div class="hamburger">
         <span class="bar"></span>
